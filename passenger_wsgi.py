e12deeb6fb506ea535d4df526ddac67944acb782
Dan Born <dan+github@danborn.net>
2019-04-25 15:36:22 -0400

    logging

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,7 +1,24 @@
 # System modules
 import flask
+import logging.config
 import os
 import sys
+
+logging.config.dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
 
 # My modules
 sys.path.insert(0, os.path.dirname(__file__))
@@ -18,6 +35,12 @@ static_versions = repeating_ical_events_http.StaticVersions(application)
 # currently '/repeating_events/'.
 @application.route('/', methods=['GET', 'POST'])
 def RepeatingEvents():
+  application.logger.info('Handling request %s', flask.request)
+  resp = ['--begin--']
+  for handler in application.logger.handlers:
+    resp.append(str(handler))
+  resp.append('--end--')
+  return '<br />'.join(resp)
   handler = repeating_ical_events_http.RequestHandler(
     uid_gens, static_versions, application, flask.request)
   return handler.Response()
